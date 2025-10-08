e9787a77a02a2a36b2c9b05f1ec4df18ef6fd9b4
Konstantin Simusev <simussev.k@icloud.com>
2025-10-08 23:34:22 +0500

    feat: added TypeORM PostgreSQL database configuration

---
+++ src/database/database.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModuleOptions> => {
+        const dbUrl = configService.get<string>('DB_ADDRESS');
+
+        return {
+          type: 'postgres',
+          url: dbUrl,
+          entities: [],
+          migrations: ['dist/migration/**/*{.js,.ts}'],
+          synchronize: false,
+          retryAttempts: 5, // Количество попыток переподключения
+          retryDelay: 1000, // Задержка между попытками в миллисекундах
+        };
+      },
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
