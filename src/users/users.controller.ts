fc37da1ba83ca3dab2927db06dfa15fbb768d1e3
DKMFzF <flim.win@yandex.ru>
2025-10-14 23:14:18 +0700

    feat: added user/me endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from 'src/entities/user.entity';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +10,11 @@ export class UsersController {
     @Get(':id')
     async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<User> {
         return this.userService.findOneById(id);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('me')
+    async getMe(@Req() req: User): Promise<User> {
+        return this.userService.findOneById(req.id);
     }
 }
