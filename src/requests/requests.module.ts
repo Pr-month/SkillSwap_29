93da8196cb48b390432542bf66bc0b5f40310402
productname <info@rocket.name>
2025-10-24 01:56:44 +0300

    fix: fix requests.module.ts

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -3,9 +3,11 @@ import { RequestsService } from './requests.service';
 import { RequestsController } from './requests.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Request } from './entities/request.entity';
+import { UsersModule } from '../users/users.module';
+import { SkillsModule } from '../skills/skills.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Request])],
+  imports: [TypeOrmModule.forFeature([Request]), UsersModule, SkillsModule],
   controllers: [RequestsController],
   providers: [RequestsService],
   exports: [RequestsService],
