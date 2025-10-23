55e644b1ed736a151fac8c5815a49471d4ba588f
productname <info@rocket.name>
2025-10-24 00:10:32 +0300

    feat: add requests DTO

---
+++ src/requests/dto/create-request.dto.ts
@@ -0,0 +1,20 @@
+import { RequestStatus } from '../enums/request-status.enum';
+import { IsEnum, IsOptional, IsUUID } from 'class-validator';
+
+export class CreateRequestDto {
+  @IsUUID()
+  receiverId: string;
+
+  @IsUUID()
+  offeredSkillId: string;
+
+  @IsUUID()
+  requestedSkillId: string;
+
+  @IsOptional()
+  @IsEnum(RequestStatus, { message: 'Некорректный статус заявки' })
+  status?: RequestStatus;
+
+  @IsOptional()
+  isRead?: boolean;
+}
