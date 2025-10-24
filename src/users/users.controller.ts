44cc7626ea0cd4e8701f1c2ab4f257c320df32c0
IvanKiryukhin19 <kirivn@yandex.ru>
2025-10-24 07:05:53 +0300

    add PasswordDto, updatePassword to controller and sevice

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Param, ParseUUIDPipe, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Param, ParseUUIDPipe, Req, UseGuards, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from 'src/entities/user.entity';
 import { AuthRequest } from 'src/auth/types';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { PasswordDto } from 'src/auth/dto/password.dto';
 
 @Controller('users')
 export class UsersController {
@@ -22,5 +23,11 @@ export class UsersController {
     @Get('me')
     async getMe(@Req() req: AuthRequest): Promise<User> {
         return this.userService.findOneById(req.user.sub);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch('me/password')
+    async updatePassword(@Req() req: AuthRequest, @Body() updatePasswordDto: PasswordDto) {
+        return this.userService.updatePassword(req.user.sub, updatePasswordDto.currentPassword, updatePasswordDto.newPassword);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { appConfig } from 'src/config/app.config';
 
 @Injectable()
 export class UsersService {
@@ -21,7 +23,22 @@ export class UsersService {
     
     return user;
   }
-    async getAllUsers(): Promise<User[]> {
-        return await this.userRepository.find();
+  
+  async getAllUsers(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async updatePassword(id:string, oldPassword: string, newPassword: string) {
+    const user = await this.findOneById(id);
+
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    
+    if (isMatch) {
+      const hashedNewPassword = await bcrypt.hash(newPassword, appConfig().bcryptSalt);
+      await this.userRepository.update(id, { password: hashedNewPassword });
+      return {message: "Пароль успешно обновлен"};
+    } else {
+      throw new NotFoundException(`Старый пароль не совпадает`);
     }
+  }
 }
