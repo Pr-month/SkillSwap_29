b104976955624670aa95f3299b94eb0f6211b6e2
productname <info@rocket.name>
2025-10-24 21:04:50 +0300

    feat: add logger

---
+++ src/logger/formats/index.ts
@@ -0,0 +1,3 @@
+export * from './console.format';
+export * from './http.format';
+export * from './tskv.format';
---
+++ src/logger/logging.config.ts
@@ -0,0 +1,14 @@
+import * as winston from 'winston';
+import {
+  consoleTransport,
+  fileJsonTransport,
+  fileTskvTransport,
+} from './transports';
+
+export const loggingConfig: winston.LoggerOptions = {
+  levels: winston.config.npm.levels,
+  transports: [
+    consoleTransport, // 1. Транспорт для консоли
+    fileJsonTransport, // 2. Транспорт для JSON-файла
+  ],
+};
