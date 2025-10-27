dc3fa3221edb500e5310497cef1aeb2159d63463
productname <info@rocket.name>
2025-10-27 21:28:52 +0300

    feat: add NODE_ENV to config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export const appConfig = registerAs('APP_CONFIG', () => ({
+  env: process.env.NODE_ENV || 'development',
   host: process.env.HOST || 'localhost',
   port: Number(process.env.PORT) || 3000,
   bcryptSalt: Number(process.env.BCRYPT_SALT) || 10,
