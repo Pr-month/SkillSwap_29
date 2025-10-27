3f18b8bd448eeeb90aef185df511c570c37e7ff5
Эмиров Нурлан Союнович <eminur@mail.ru>
2025-10-27 14:32:40 +0500

    Обновление данных текущего пользователя по эндпоинту PATCH

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Param, ParseUUIDPipe, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe, Req, UseGuards,Patch , Body} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from 'src/entities/user.entity';
 import { AuthRequest } from 'src/auth/types';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -23,4 +24,13 @@ export class UsersController {
     async getMe(@Req() req: AuthRequest): Promise<User> {
         return this.userService.findOneById(req.user.sub);
     }
+    
+    @UseGuards(JwtAuthGuard)
+    @Patch('me')
+    async updateMe(
+    @Req() req: AuthRequest,
+    @Body() updateUserDto: UpdateUserDto,
+    ): Promise<User> {
+    return this.userService.updateUser(req.user.sub, updateUserDto);
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -18,10 +19,23 @@ export class UsersService {
     if (!user) {
       throw new NotFoundException(`User with ID ${id} not found`);
     }
-    
+
     return user;
   }
-    async getAllUsers(): Promise<User[]> {
-        return await this.userRepository.find();
+  async getAllUsers(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async updateUser(id: string, updateData: UpdateUserDto): Promise<User> {
+    const user = await this.userRepository.preload({
+      id,
+      ...updateData,
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
     }
+
+    return this.userRepository.save(user);
+  }
 }
