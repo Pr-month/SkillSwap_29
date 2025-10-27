f2c952e48ba0ce3e8885250e3405a40519f3a8f2
unknown <dima.mr-smith@yandex.ru>
2025-10-27 14:15:55 +0300

    fix: Не получается открыть изображение на сервере

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppService } from './app.service';
 import { appConfig } from './config/app.config';
 import { jwtConfig } from './config/jwt.config';
@@ -15,9 +16,14 @@ import { SkillsModule } from './skills/skills.module';
 import { CategoriesModule } from './categories/categories.module';
 
 import { FilesModule } from './files/files.module';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/public',
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig, jwtConfig, dbConfig, fileConfig],
