12ac22cf2ad30c27d6bd707bdf2b64b73343ac9a
productname <info@rocket.name>
2025-10-31 18:32:29 +0300

    fix: fix jwt-auth linting

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -8,32 +8,43 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { jwtConfig } from 'src/config/jwt.config';
 import { IJwtConfig } from 'src/config/types';
+import { Request } from 'express';
+import { UserPayload } from '@/auth/guards/roles.guard'; // Импортируем тип Request из express
+
+export interface RequestWithUser extends Request {
+  user?: UserPayload;
+}
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(
-    private jwtService: JwtService,
+    private readonly jwtService: JwtService,
     @Inject(jwtConfig.KEY)
     private readonly jwtConfig: IJwtConfig,
   ) {}
 
-  canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest();
-    const authHeader = request.headers.authorization;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const token = this.extractTokenFromHeader(request);
 
-    if (!authHeader)
-      throw new UnauthorizedException('Authorization header missing');
-
-    const [, token] = authHeader.split(' ');
-    if (!token) throw new UnauthorizedException('Token missing');
+    if (!token) {
+      throw new UnauthorizedException('Token not found');
+    }
 
     try {
-      request.user = this.jwtService.verify(token, {
+      const payload = await this.jwtService.verifyAsync<UserPayload>(token, {
         secret: this.jwtConfig.accessSecret,
       });
-      return true;
+
+      request.user = payload;
     } catch {
       throw new UnauthorizedException('Invalid or expired token');
     }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
