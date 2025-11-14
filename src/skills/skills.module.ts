4ad1e118aa729b22b8fa0ae80a795d2559a56f3a
Roland <machodin@yandex.ru>
2025-11-14 21:57:56 +0300

    removed unused imports

--- src/skills/skills.module.ts
+++ src/skills/skills.module.ts
@@ -1,15 +1,15 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { SkillsService } from './skills.service';
-import { SkillsController } from './skills.controller';
-import { UsersModule } from '@/users/users.module';
 import { Category } from '@/entities/category.entity';
 import { Skill } from '@/entities/skill.entity';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SkillsController } from './skills.controller';
+import { SkillsService } from './skills.service';
+import { User } from '@/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Skill, Category]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Skill, Category, User])],
   controllers: [SkillsController],
   providers: [SkillsService],
-  exports: [SkillsService, TypeOrmModule],
+  exports: [SkillsService],
 })
-export class SkillsModule {}
+export class SkillsModule { }
